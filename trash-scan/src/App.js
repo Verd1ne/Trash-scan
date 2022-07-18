2cb0e79799061a20985ff4e9b25775e66d1aad48
LeonnR <101970574+LeonnR@users.noreply.github.com>
2022-07-18 20:02:42 +0700

    page1 design

--- trash-scan/src/App.js
+++ trash-scan/src/App.js
@@ -1,19 +1,42 @@
 import './App.css';
+import Logo from "../src/trashscan-icon.png";
+import Card from "../src/card-template.png";
+import Card2 from "../src/DisplayCard1.png";
 
 function App() {
   return (
-    <div classname="main">
-      <div className="buttons">
-        <button class="button is-primary" className="text-3xl font-bold underline">Primary</button>
-        <button class="button is-link">Link</button>
-        <button class="button is-info">Info</button>
-        <button class="button is-success">Success</button>
-        <button class="button is-warning">Warning</button>
-        <button class="button is-danger">Danger</button>
-        <button class="button is-black">Black</button>
-        <button class="button is-white">White</button>
-        <button class="button is-dark">Dark</button>
-        <button class="button is-light">Light</button>
+    <div
+      style={{
+        display: "",
+        flexDirection: "",
+        alignItems: "",
+        backgroundColor: "black",
+      }}
+    >
+      <title>TrashScan</title>
+      <div className=" ">
+          <div>
+          <header className=" bg-yellow-300 rounded-3xl rounded-t-none pt-4">
+              <img src={Logo} className="object-contain h-16 w-16 flex ml-72 pb-2 " ></img>
+          </header>
+
+        </div>
+        <div className="h-60">
+            <img src={Card} className="GreyCard rounded-3xl flex object-contain"></img>
+            <div className="BlackBg block bg-black mt-5">
+            </div>
+        </div>
+        <br />
+        <div>
+            <section className="LowerGradient block bg-gray-500 h-96 pt-9">
+              <div> 
+                <img src={Card2} className="displaycard1 rounded-3xl object-center"></img>
+              </div>
+           
+            </section>
+        </div>
+       
+       
       </div>
     </div>
   );
