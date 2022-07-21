a0833317bf147a81285737c6bffa7e6d8f2b35a7
Verd1ne <76729013+Verd1ne@users.noreply.github.com>
2022-07-21 21:41:21 +0700

    Revert "Merge pull request #7 from Verd1ne/Verdine"
    This reverts commit ae0554071d9c402e74d7bfbb60edfe7e7f4f4f15, reversing
    changes made to 30f835e769446995b39953b2e58018dd1ae8cd21.

--- trash-scan/src/App.js
+++ trash-scan/src/App.js
@@ -1,41 +1,21 @@
-import React, { useEffect,useState } from "react";
-import HeroSection from './pages/HeroSection';
-import BalanceSection from './pages/BalanceSection'
-import Card from "../src/components/Card";
-import Card2 from "./images/displaycard1.png";
-import "./styles/global.scss";
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-
-AOS.init({
-  // Global settings:
-  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-  startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-  initClassName: 'aos-init', // class applied after initialization
-  animatedClassName: 'aos-animate', // class applied on animation
-  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-});
+import React from 'react'
+import './App.css';
+import Home from './pages/Home';
+import Scan from './pages/Scan';
+import {  BrowserRouter as Router,  Routes,  Route} from "react-router-dom";
 
 function App() {
-  const [hideMenu,showMenu] = useState(false)
-  useEffect(() => {
-    window.addEventListener('scroll',listenToScroll)
-  }, [])
-  function listenToScroll(){
-    if(window.pageYOffset >= 700){
-      showMenu(true)
-    }else{
-      showMenu(false)
-    }
-  }
   return (
     <div>
-      <HeroSection/>
-      <BalanceSection/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/scan" element={<Scan />} />
+        </Routes>
+      </Router>
+      
     </div>
-    );
+  );
 }
+
 export default App;
